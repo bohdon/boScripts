2c68d98cf5a9a07c91c4788c5866737d3bdfecf5
Bohdon Sayre <bohdon@gmail.com>
2011-05-05 00:45:36 -0500

    create smear is about half done

--- src/boSmear.py
+++ src/boSmear.py
@@ -23,6 +23,9 @@ from pymel.core import *
 
 __version__ = '3.0.0dev'
 
+class SmearError(Exception):
+    pass
+
 class Gui(object):
     camera_ann = 'The camera on which to apply the smear. This can be changed later.'
     target_ann = 'The object to fix the center of the smear deformer to.'
@@ -71,7 +74,7 @@ class Gui(object):
                                     (btn1, 'top', 0), (btn1, 'right', 0), (btn2, 'right', 0)],
                                 ac=[(geoList, 'right', 2, btn1), (btn2, 'top', 0, btn1)],
                             )
-                    btn1 = button(l='Create Smear', h=30, bgc=(0.75, 0.75, 0.75))
+                    btn1 = button(l='Create Smear', h=30, bgc=(0.75, 0.75, 0.75), c=Callback(self.create))
                     btn2 = button(l='Control Window', h=30, w=100, c=Callback(ControlGui))
                     formLayout(mainForm, e=True,
                         af=[(stngsFrame, 'top', 0), (stngsFrame, 'left', 0), (stngsFrame, 'right', 0),
@@ -124,6 +127,14 @@ class Gui(object):
         items = self.uiGeoList.getSelectItem()
         for item in items:
             self.uiGeoList.removeItem(item)
+    
+    def create(self):
+        cam = PyNode(self.uiCamMenu.getValue())
+        target = PyNode(self.uiTargetField.getText())
+        res = [self.uiResXField.getValue(), self.uiResYField.getValue()]
+        geo = [PyNode(x) for x in self.uiGeoList.getAllItems()]
+        s = Smear(cam, target, res, geo)
+        s.create()
 
 
 class ControlGui(object):
@@ -133,4 +144,58 @@ class ControlGui(object):
         
         with window('bsmControlWin', t='Smear Control {0}'.format(__version__)):
             pass
+
+
+
+class Smear(object):
+    def __init__(self, cam, target, res, geo=None):
+        """
+        ``cam`` -- the camera to attach the smear mesh to
+        ``target`` -- the node to attach the center of the deformer to
+        ``res`` -- the resolution of the smear mesh ie. (18, 12)
+        ``geo`` -- a list of all geometry to be affected by the smear
+        """
+        self.cam = cam
+        self.target = target
+        self.res = res
+        self.geo = geo
+    
+    def create(self):
+        self._validateObjs()
+        self._createHierarchy()
+        self._createLattice()
+        #self.createAttrs()
+        #self.createMesh()
+    
+    def _validateObjs(self):
+        pass
+    
+    def _createHierarchy(self):
+        self.mainGrp = group(em=True, n='smear_grp#')
+        self.camChild = group(em=True, n='smear_camChild', p=self.mainGrp)
+        self.meshOffset = group(em=True, n='smear_mesh_offset', p=self.camChild)
+        self.meshScale = group(em=True, n='smear_mesh_scale', p=self.meshOffset)
+        self.latticeFollow = group(em=True, n='smear_lattice_follow', p=self.camChild)
+        self.latticeScale = group(em=True, n='smear_lattice_scale', p=self.latticeFollow)
+    
+    def _createLattice(self):
+        ffd, self.lattice, base = lattice(ignoreSelected=True, dv=self.res + [2], ldv=(4, 4, 4), cp=True, s=(1, 1, 1), n='smear#', pos=(0, 0, 0))
+        self.latticeGrp = self.lattice.getParent()
+        # when a lattice is made, the vertex points are absolute, if a deformer
+        # (such as a cluster) is applied a latticeShapeOrig is created which
+        # maintains the absolute positions. the deformer could then be deleted
+        # and the lattice points would remain zeroed. this is necessary for us
+        # to connect them to the mesh points which will also be zeroed
+        select(self.lattice.pt[0][0][0])
+        newC = cluster(relative=True, envelope=1)
+        delete(newC)
+        #move into hierarchy
+        parent(self.latticeGrp, self.latticeScale)
+        resX, resY = self.res
+        sX = 1.0275 * float(resX+2)/resX
+        sY = 0.58 * float(resY+2)/resY
+        self.latticeGrp.scaleX.set(sX)
+        self.latticeGrp.scaleY.set(sY)
+        self.latticeGrp.visibility.set(False)
+
 
